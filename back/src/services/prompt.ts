058692611a693eea339eabb4fc1795a8d106ed8e
Niklas Stangl <67476141+nStangl@users.noreply.github.com>
2024-11-23 19:32:25 +0100

    update prompt for raw json

--- back/src/services/prompt.ts
+++ back/src/services/prompt.ts
@@ -23,14 +23,14 @@ function createArticleGenerationPrompt(article_contents: string[], keywords: str
 
     if (language === Language.German) {
         const prompt = `Du bist ein Autor für eine deutsche Nachrichtenwebsite für Elektrofahrzeuge. Schreibe einen Artikel mit ${length} Wörtern. Der Stil sollte ein formeller Nachrichtenbeitrag sein. Versuche die folgenden Schlüsselwörter einzubeziehen: ${keywords.join(", ")}.\n\n` +
-            `Deine Antwort muss valides JSON sein. Es soll einen Titel haben (json: title), eine Unterüberschrift (json: subheadline), eine knappe und aufmerksamkeitserregende Einleitung (json: lead), und den eigentlichen Hauptteil (json: body).\n\n` +
+            `Deine Antwort muss valides JSON sein. Verwende keine Markdown Syntax, die Antwort muss in Klartext sein, da wir es als JSON parsen müssen. Es soll einen Titel haben (json: title), eine Unterüberschrift (json: subheadline), eine knappe und aufmerksamkeitserregende Einleitung (json: lead), und den eigentlichen Hauptteil (json: body).\n\n` +
             `Verwende die folgenden Texte als Vorlage:\n${formattedContents}`
 
         return { language, content: prompt };
     }
 
     const prompt = `You are a writer for a German news website about electric vehicles. Write it in english. Write an article with ${length} words. The style should be a formal news report. Try to include the following keywords: ${keywords.join(", ")}.\n\n` +
-        `Your response smust be valid JSON. It should have a title (json: title), a subheadline (json: subheadline), a concise and attention-grabbing introduction (json: lead), and the main body of the article (json: body).\n\n` +
+        `Your response smust be valid JSON. Do not use Markdown syntax, the response must be in cleartext, as we have to parse it as JSON. It should have a title (json: title), a subheadline (json: subheadline), a concise and attention-grabbing introduction (json: lead), and the main body of the article (json: body).\n\n` +
         `Use the following texts as a template:\n${formattedContents}`;
 
     return { language, content: prompt };
