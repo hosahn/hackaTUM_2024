e7a40b44d5905a9e595d3f432c5193735849160d
Niklas Stangl <67476141+nStangl@users.noreply.github.com>
2024-11-23 16:19:36 +0100

    feat(api): implemented api/generateArticle

--- back/src/routers/mainRouter.ts
+++ back/src/routers/mainRouter.ts
@@ -1,11 +1,12 @@
-import express, { Router, Request, Response , Application } from 'express';
+import express, { Router, Request, Response, Application } from 'express';
 import { basicAIService } from '../services/basicAI';
 import { GenericUtilService } from '../services/genericUtil';
-import { Aggregator, Topic } from '../lib/aggregator';
+import { Aggregator, Topic } from '../services/aggregator';
+import { Language, ArticleLength } from '../services/prompt';
 import { Summary } from '../services/basicAI';
-const mainRouter:Router = Router();
-const aggregator:Aggregator = new Aggregator();
-mainRouter.get("/api", async(req:Request, res:Response)  => {
+const mainRouter: Router = Router();
+const aggregator: Aggregator = new Aggregator();
+mainRouter.get("/api", async (req: Request, res: Response) => {
     await basicAIService.generateMedia(["boy", "asian", "anime"])
     res.send("Welcome to news generator. Every requests should go through post request")
 })
@@ -25,7 +26,7 @@ mainRouter.post("/api/getArticles", async(req:Request, res:Response)  => {
         "https://www.autobild.de/rss/22590661.xml"]
     var result = await aggregator.fetchTopics(list);
     let length = result.length > 10 ? 10 : result.length
-    for(let i = 0; i < length; i++){
+    for (let i = 0; i < length; i++) {
         var actual_summary = GenericUtilService.extractArticleFromUrl(result[i].link);
         result[i].content = await actual_summary || result[i].content;
     }
@@ -44,9 +45,36 @@ mainRouter.post("/api/getArticles", async(req:Request, res:Response)  => {
     res.send(combined);
 })
 
-mainRouter.post("/api/generateArticle", async(req:Request, res:Response)  => {
-    // TODO var result = await basicAIService.generateArticles(req.body);
-    var result = await basicAIService.generateArticles("");
+mainRouter.post("/api/generateArticle", async (req: Request, res: Response) => {
+    // var selected_urls: string[] = ["https://www.t-online.de/mobilitaet/aktuelles/id_100537168/tesla-cybercab-kommt-nach-europa-hier-sehen-sie-das-elon-musk-robotaxi.html",
+    //     "https://www.chip.de/news/In-Deutschland-gibt-es-jetzt-den-ersten-reinen-Ladepark-fuer-E-Autos_185625526.html",
+    //     "https://www.merkur.de/wirtschaft/das-naechste-ampel-projekt-fuer-deutschland-in-gefahr-europas-hoffnungstraeger-ist-insolvent-zr-93426508.html"
+    // ];
+    // var keywords: string[] = ["Technology", "Electric Vehicles", "Tesla", "Elon Musk", "Germany", "Europe"];
+    // var word_count: number = 250;
+
+    var selected_urls: string[] = req.body.urls;
+    var keywords: string[] = req.body.keywords;
+    var word_count: number = req.body.word_count;
+
+    var length: ArticleLength;
+    switch (word_count) {
+        case 250:
+            length = ArticleLength.SHORT;
+            break;
+        case 500:
+            length = ArticleLength.MEDIUM;
+            break;
+        case 1000:
+            length = ArticleLength.LONG;
+            break;
+        default:
+            console.log("Invalid word count, defaulting to medium length.");
+            length = ArticleLength.MEDIUM;
+            break;
+    }
+
+    var result = await basicAIService.generateArticles(selected_urls, keywords, length, Language.German);
     var final_result = await basicAIService.automatedQualityCheck(result);
 
     var what_does_people_think = await GenericUtilService.cool_keyword_twitter_scraper(["",""])
@@ -54,7 +82,7 @@ mainRouter.post("/api/generateArticle", async(req:Request, res:Response)  => {
     res.send(final_result)
 })
 
-mainRouter.post("/api/userFeedback", async(req:Request, res:Response) => {
+mainRouter.post("/api/userFeedback", async (req: Request, res: Response) => {
     res.send("1337")
 })
 
@@ -63,7 +91,7 @@ mainRouter.post("/api/generateImages", async(req:Request, res:Response) => {
     res.send(imageList)
 })
 
-mainRouter.post("/api/publishArticle", async(req:Request,res:Response)=>{
+mainRouter.post("/api/publishArticle", async (req: Request, res: Response) => {
     res.send("1337")
 })
 
