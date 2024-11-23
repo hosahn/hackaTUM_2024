927d34d9978743d28f6c61cd697ca99658616ede
Hosan Lee (fernao) <hosan.lee@optimabit.com>
2024-11-23 01:33:02 +0100

    added basic openAPI

--- back/src/routers/mainRouter.ts
+++ back/src/routers/mainRouter.ts
@@ -4,8 +4,8 @@ import { basicAIService } from '../services/basicAI';
 const mainRouter:Router = Router();
 
 mainRouter.get("/main", async(req:Request, res:Response)  => {
-    var result = await basicAIService.searchArticles();
-    res.send("Initial endpoint");
+    var result = await basicAIService.searchArticles("hello!");
+    res.send(result);
 })
 
 export { mainRouter }
--- back/src/services/basicAI.ts
+++ back/src/services/basicAI.ts
@@ -1,6 +1,25 @@
+import dotenv from 'dotenv';
+import axios from "axios";
+
+dotenv.config();
+
+const AZURE_ENDPOINT = 'https://hackatum-2024.openai.azure.com/openai/deployments/gpt-4o/chat/completions?api-version=2024-08-01-preview&API-KEY=2bBZn5pMnk003jcxoOLmZV9lKLflifdjC8mOOSpep0wB9lIaDoAyJQQJ99AKACfhMk5XJ3w3AAABACOGPo7V&content-type=application/json';
+
 class basicAIService{
-    static async searchArticles(){
-        return ""
+    
+    static async searchArticles(input:string){
+
+        try {
+            const api_call = await axios.post(
+                AZURE_ENDPOINT,
+                { "messages": [{"role": "user", "content": "who will be the next president of US?"}], },
+            );
+    
+            const responseMessage = api_call.data.choices[0].message.content;
+            return responseMessage;
+        } catch (error:any) {
+            console.error('Error:', error.response?.data || error.message);
+        }
     }
 }
 
