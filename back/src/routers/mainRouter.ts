62202e4fd300b10e5b568851c826a997208fa0c8
Niklas Stangl <67476141+nStangl@users.noreply.github.com>
2024-11-23 16:19:36 +0100

    feat(api): implemented api/generateArticle

--- back/src/routers/mainRouter.ts
+++ back/src/routers/mainRouter.ts
@@ -1,49 +1,77 @@
-import express, { Router, Request, Response , Application } from 'express';
+import express, { Router, Request, Response, Application } from 'express';
 import { basicAIService } from '../services/basicAI';
 import { GenericUtilService } from '../services/genericUtil';
-import { Aggregator, Topic } from '../lib/aggregator';
-const mainRouter:Router = Router();
-const aggregator:Aggregator = new Aggregator();
-mainRouter.get("/api", async(req:Request, res:Response)  => {
+import { Aggregator, Topic } from '../services/aggregator';
+import { Language, ArticleLength } from '../services/prompt';
+const mainRouter: Router = Router();
+const aggregator: Aggregator = new Aggregator();
+mainRouter.get("/api", async (req: Request, res: Response) => {
     await basicAIService.generateMedia(["boy", "asian", "anime"])
     res.send("Welcome to news generator. Every requests should go through post request")
 })
 
 interface getArticlesView {
-    metainfo : Topic,
-    summary:string,
+    metainfo: Topic,
+    summary: string,
 }
-mainRouter.get("/api/getArticles", async(req:Request, res:Response)  => {
+mainRouter.get("/api/getArticles", async (req: Request, res: Response) => {
     var list = ["https://rss.app/feeds/MLuDKqkwFtd2tuMr.xml",
         "https://www.autobild.de/rss/22590661.xml"]
     var result = await aggregator.fetchTopics(list);
     let length = result.length > 10 ? 10 : result.length
-    for(let i = 0; i < length; i++){
+    for (let i = 0; i < length; i++) {
         var actual_summary = GenericUtilService.extractArticleFromUrl(result[i].link);
         result[i].content = await actual_summary || result[i].content;
     }
 
     var summaries = await basicAIService.summaryArticles(result);
-    var combined : getArticlesView[] = [];
-    for(let i = 0; i < summaries.length; i++){
-        let tmp : getArticlesView = {metainfo:result[i],summary:summaries[i]};
+    var combined: getArticlesView[] = [];
+    for (let i = 0; i < summaries.length; i++) {
+        let tmp: getArticlesView = { metainfo: result[i], summary: summaries[i] };
         combined.push(tmp);
     }
     res.send(combined);
 })
 
-mainRouter.post("/api/generateArticle", async(req:Request, res:Response)  => {
-    // TODO var result = await basicAIService.generateArticles(req.body);
-    var result = await basicAIService.generateArticles("");
+mainRouter.post("/api/generateArticle", async (req: Request, res: Response) => {
+    // var selected_urls: string[] = ["https://www.t-online.de/mobilitaet/aktuelles/id_100537168/tesla-cybercab-kommt-nach-europa-hier-sehen-sie-das-elon-musk-robotaxi.html",
+    //     "https://www.chip.de/news/In-Deutschland-gibt-es-jetzt-den-ersten-reinen-Ladepark-fuer-E-Autos_185625526.html",
+    //     "https://www.merkur.de/wirtschaft/das-naechste-ampel-projekt-fuer-deutschland-in-gefahr-europas-hoffnungstraeger-ist-insolvent-zr-93426508.html"
+    // ];
+    // var keywords: string[] = ["Technology", "Electric Vehicles", "Tesla", "Elon Musk", "Germany", "Europe"];
+    // var word_count: number = 250;
+
+    var selected_urls: string[] = req.body.urls;
+    var keywords: string[] = req.body.keywords;
+    var word_count: number = req.body.word_count;
+
+    var length: ArticleLength;
+    switch (word_count) {
+        case 250:
+            length = ArticleLength.SHORT;
+            break;
+        case 500:
+            length = ArticleLength.MEDIUM;
+            break;
+        case 1000:
+            length = ArticleLength.LONG;
+            break;
+        default:
+            console.log("Invalid word count, defaulting to medium length.");
+            length = ArticleLength.MEDIUM;
+            break;
+    }
+
+    var result = await basicAIService.generateArticles(selected_urls, keywords, length, Language.German);
     var final_result = await basicAIService.automatedQualityCheck(result);
     res.send(final_result)
 })
 
-mainRouter.post("/api/userFeedback", async(req:Request, res:Response) => {
+mainRouter.post("/api/userFeedback", async (req: Request, res: Response) => {
     res.send("1337")
 })
 
-mainRouter.post("/api/publishArticle", async(req:Request,res:Response)=>{
+mainRouter.post("/api/publishArticle", async (req: Request, res: Response) => {
     res.send("1337")
 })
 
--- back/src/services/basicAI.ts
+++ back/src/services/basicAI.ts
@@ -1,60 +1,73 @@
 import dotenv from 'dotenv';
 import axios from "axios";
-import { Topic } from '../lib/aggregator';
+import { Topic } from './aggregator';
+import { GenericUtilService } from './genericUtil';
+import { ArticleLength, Language, createArticleGenerationPrompt } from './prompt';
 dotenv.config();
 
 const AZURE_ENDPOINT = process.env.GPT_4o || "none"
 const AZURE_IMAGE_ENDPOINT = process.env.GPT_IMAGE || "none"
 
-class basicAIService{
-    
-    static async generateArticles(input:string){
+class basicAIService {
+
+    static async generateArticles(urls: string[], keywords: string[], word_count: number, language: Language) {
+        const contents = await Promise.all(urls.map(url =>
+            GenericUtilService.extractArticleFromUrl(url)
+        ));
+        // Drop articles with no content
+        const filtered_contents = contents.filter(content => content !== null) as string[];
+        const prompt = createArticleGenerationPrompt(filtered_contents, keywords, word_count, Language.German);
+        console.log("Prompt:", prompt.content);
 
         try {
             const api_call = await axios.post(
                 AZURE_ENDPOINT,
-                { "messages": [{"role": "user", "content": "who will be the next president of US?"}], },
+                { "messages": [{ "role": "user", "content": prompt.content }], },
             );
-    
+
             const responseMessage = api_call.data.choices[0].message.content;
             return responseMessage;
-        } catch (error:any) {
+        } catch (error: any) {
             console.error('Error:', error.response?.data || error.message);
         }
     }
-    static async summaryArticles(input:Topic[]){
+
+    static async summaryArticles(input: Topic[]) {
         input.sort((a, b) => {
             if (a.pubDate && b.pubDate) {
                 const dateA = new Date(a.pubDate);
                 const dateB = new Date(b.pubDate);
                 return dateB.getTime() - dateA.getTime();
             }
-            return 0; 
+            return 0;
         });
 
-        let result : string[] = []
+        let result: string[] = []
 
-        
+
         let length = input.length > 10 ? 10 : input.length
-        for(let i = 0; i < length; i++){
+        for (let i = 0; i < length; i++) {
             let text = input[i].content;
             try {
                 const api_call = await axios.post(
                     AZURE_ENDPOINT,
                     // prompt engineering 
-                    { "messages": [{"role": "user", "content": `Please get me a short summary (max. 2 sentences) of this article:${text} Don't answer with sure! or something like that. Please get straight into the point. And all of your result should be in English`
-                    }], },
+                    {
+                        "messages": [{
+                            "role": "user", "content": `Please get me a short summary (max. 2 sentences) of this article:${text} Don't answer with sure! or something like that. Please get straight to the point. And all of your result should be in English`
+                        }],
+                    },
                 );
                 const responseMessage = api_call.data.choices[0].message.content;
                 result.push(responseMessage);
-            } catch (error:any) {
+            } catch (error: any) {
                 console.error('Error:', error.response?.data || error.message);
             }
         }
         return result
     }
-    
-    static async generateMedia(keywords:string[]){
+
+    static async generateMedia(keywords: string[]) {
         try {
             const api_call = await axios.post(
                 AZURE_IMAGE_ENDPOINT,
@@ -62,21 +75,21 @@ class basicAIService{
                     "prompt": "electric car",
                     "n": 1,
                     "quality": "standard"
-                   },
+                },
             );
             const imageUrl = api_call.data.data[0].url;
             console.log(imageUrl);
             return imageUrl;
-        } catch (error:any) {
+        } catch (error: any) {
             console.error('Error:', error.response?.data || error.message);
         }
         return ""
     }
 
-    static async automatedQualityCheck(result:string){
+    static async automatedQualityCheck(result: string) {
         // TODO implement QC mecnahism
         return result;
     }
 }
 
-export {basicAIService}
+export { basicAIService }
